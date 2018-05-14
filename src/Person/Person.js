2459d4a1146e7b20164544dda4cf599cc60a551c
marian-daogaru <marian@phyramid.com>
2018-05-14 17:35:57 +0300

    l69

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import classes from './Person.css'
 
 const person = (props) => {
 
   return (
     <div
-      className='Person'
+      className={classes.Person}
     >
       <p onClick={props.click}>Im a {props.name} and Im {props.age} yo</p>
       <p>{props.children}</p>
